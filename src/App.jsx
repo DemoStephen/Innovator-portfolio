2dec2854e7aa3c8fa237e75aa0ba246486f18426
Adewale Stephen <adewalestephennifemi@gmail.com>
2025-02-23 17:40:44 +0100

    I did something okay. OKay?

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
-function App() {
+import Home from "./Pages/Home";
+
+export default function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello Innovator!</h1>
+      <h1 className="text-3xl font-bold underline text-center">
+        Hello Innovator!
+      </h1>
+      <Home />
     </>
   );
 }
-
-export default App;

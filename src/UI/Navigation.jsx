d33aba6ec2568874b605b91f53d37a66b3a6473f
Adewale Stephen <adewalestephennifemi@gmail.com>
2025-03-04 03:54:19 +0100

    I cant even remember what I touched

--- src/UI/Navigation.jsx
+++ src/UI/Navigation.jsx
@@ -2,7 +2,7 @@ import List from "../Components/List";
 
 export default function Navigation() {
   return (
-    <nav className="bg-black text-white flex items-center justify-between px-[5%] py-6">
+    <nav className="bg-black text-white flex items-center justify-between px-[5%] py-7">
       <p className="text-3xl">Innovator</p>
       <ul className="flex items-center gap-8 text-xl">
         <List content="Design" />

0c6a3b9a71ca347929cbf18e6d8ba288cde17c10
Adewale Stephen <adewalestephennifemi@gmail.com>
2025-03-04 04:23:17 +0100

    navigation responsiveness worked on

--- src/UI/Navigation.jsx
+++ src/UI/Navigation.jsx
@@ -2,9 +2,9 @@ import List from "../Components/List";
 
 export default function Navigation() {
   return (
-    <nav className="bg-black text-white flex items-center justify-between px-[5%] py-7">
+    <nav className="bg-black text-white flex items-center justify-between px-[5%] py-7 sticky top-0">
       <p className="text-3xl">Innovator</p>
-      <ul className="flex items-center gap-8 text-xl">
+      <ul className="hidden md:flex items-center gap-8 text-xl">
         <List content="Design" />
         <List content="Photo" />
         <List content="Art" />
@@ -12,6 +12,17 @@ export default function Navigation() {
         <List content="About" />
         <List content="Contact" />
       </ul>
+      <button className="md:hidden">menu</button>
+      <section className="absolute top-[5.375rem] left-0 right-0 h-[calc(100vh-5.375rem)] bg-black flex px-[5%] md:hidden">
+        <ul className="flex flex-col w-full justify-center gap-8 text-xl ">
+          <List content="Design" />
+          <List content="Photo" />
+          <List content="Art" />
+          <List content="Music" />
+          <List content="About" />
+          <List content="Contact" />
+        </ul>
+      </section>
     </nav>
   );
 }
